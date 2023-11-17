b63935a09026b984f18b215bbdd8712e5e675a4e
penguinify <103973145+Penguinify@users.noreply.github.com>
2023-11-16 16:21:50 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://penguin-movies.penguinifyminecraft.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjVkMzRlZmU4YmJmMDBjMDUyYTQyNzNkZDJlNWIzMCIsInN1YiI6IjY1NTZiMTM1N2YwNTQwMDBlMmFmNGY0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1vSKhhk0u_fZ6q98WF6G4yJ964QVdYytd9K0qD568b4"
 };
